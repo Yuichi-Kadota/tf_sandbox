fb4c1cd76a7b2e7643cbc6fe55f4b95e4c6cdec5
Yuichi-Kadota <yuichi_kadota@mediado.jp>
2022-09-19 11:56:41 +0900

    wdのoutput追加

--- network/dev/output.tf
+++ network/dev/output.tf
@@ -9,3 +9,39 @@ output "vpc_id" {
 output "vpc_arn" {
   value = module.vpc.vpc_arn
 }
+
+################################################################################
+# Subnet
+################################################################################
+
+output "pub_sub_a_id" {
+  value = module.subnet.pub_sub_a_id
+}
+
+output "pub_sub_c_id" {
+  value = module.subnet.pub_sub_c_id
+}
+
+output "pri_sub_a_id" {
+  value = module.subnet.pub_sub_a_id
+}
+
+output "pri_sub_c_id" {
+  value = module.subnet.pub_sub_c_id
+}
+
+output "pub_sub_a_arn" {
+  value = module.subnet.pub_sub_a_arn
+}
+
+output "pub_sub_c_arn" {
+  value = module.subnet.pub_sub_c_arn
+}
+
+output "pri_sub_a_arn" {
+  value = module.subnet.pub_sub_a_arn
+}
+
+output "pri_sub_c_arn" {
+  value = module.subnet.pub_sub_c_arn
+}
